419161a1b2aee5af4c6ac9c4e5ee26f0caa20f09
Alex Marandon <contact@alexmarandon.com>
2011-07-11 21:22:15 +0700

    A simple URL dispatcher

---
+++ src/greeting_views.erl
@@ -0,0 +1,23 @@
+-module(greeting_views).
+-compile(export_all).
+-import(greeting_shortcuts, [render_ok/3]).
+
+urls() -> [
+      {"^hello/?$", hello},
+      {"^hello/(.+?)/?$", hello}
+    ].
+
+hello('GET', Req) ->
+    QueryStringData = Req:parse_qs(),
+    Username = proplists:get_value("username", QueryStringData, "Anonymous"),
+    render_ok(Req, greeting_dtl, [{username, Username}]);
+hello('POST', Req) ->
+    PostData = Req:parse_post(),
+    Username = proplists:get_value("username", PostData, "Anonymous"),
+    render_ok(Req, greeting_dtl, [{username, Username}]).
+
+hello('GET', Req, Username) ->
+    render_ok(Req, greeting_dtl, [{username, Username}]);
+hello('POST', Req, _) ->
+  % Ignore URL parameter if it's a POST
+  hello('POST', Req).
